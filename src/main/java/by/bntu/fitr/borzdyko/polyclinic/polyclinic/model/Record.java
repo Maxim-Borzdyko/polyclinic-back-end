4968d1d481b3b84643551410e103997ada0e2dca
Maxim Borzdyko <maxim.borzdyko@gmail.com>
2021-02-22 22:07:36 +0300

    GenerationType was fixed

--- src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/model/Record.java
+++ src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/model/Record.java
@@ -15,7 +15,7 @@ import java.sql.Date;
 public class Record {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
 
