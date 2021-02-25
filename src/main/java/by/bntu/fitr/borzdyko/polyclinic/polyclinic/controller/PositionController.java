7b76b1f47f275f2b91569e32df68c9984dd1ba30
Maxim Borzdyko <maxim.borzdyko@gmail.com>
2021-02-25 14:49:27 +0300

    Position functionality was added

--- src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/controller/PositionController.java
+++ src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/controller/PositionController.java
@@ -3,9 +3,8 @@ package by.bntu.fitr.borzdyko.polyclinic.polyclinic.controller;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.model.Position;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.service.PositionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,20 @@ public class PositionController {
     @GetMapping("/all")
     public List<Position> getAll() {
         return positionService.getAll();
+    }
+
+    @PostMapping("/add")
+    public Position addPosition(@RequestBody Position position) {
+        return positionService.addPosition(position);
+    }
+
+    @PostMapping("/update")
+    public Position updatePosition(@RequestBody Position position) {
+        return positionService.update(position);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deletePosition(@PathVariable("id") Position position) {
+        positionService.delete(position);
     }
 }
--- src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/service/PositionService.java
+++ src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/service/PositionService.java
@@ -3,14 +3,16 @@ package by.bntu.fitr.borzdyko.polyclinic.polyclinic.service;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.model.Position;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.repository.PositionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PositionService {
 
-    private PositionRepository positionRepository;
+    private final PositionRepository positionRepository;
 
     @Autowired
     public PositionService(PositionRepository positionRepository) {
@@ -19,6 +21,23 @@ public class PositionService {
 
     public List<Position> getAll() {
         return positionRepository.findAll();
+    }
+
+    public Position addPosition(Position position) {
+
+        if(positionRepository.findOne(Example.of(position)).isPresent()) {
+            return null;
+        }
+
+        return positionRepository.save(position);
+    }
+
+    public Position update(Position position) {
+        return positionRepository.save(position);
+    }
+
+    public void delete(Position position) {
+        positionRepository.delete(position);
     }
 
 }
