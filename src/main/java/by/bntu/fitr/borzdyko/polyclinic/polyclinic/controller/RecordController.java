45daa676fc2e9c53cb4049e319fe758e8fa1db43
Maxim Borzdyko <maxim.borzdyko@gmail.com>
2021-02-25 14:49:55 +0300

    Record functionality was added

--- src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/controller/RecordController.java
+++ src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/controller/RecordController.java
@@ -3,9 +3,8 @@ package by.bntu.fitr.borzdyko.polyclinic.polyclinic.controller;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.model.Record;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.service.RecordService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,9 +24,18 @@ public class RecordController {
         return recordService.getAll();
     }
 
-    // @RequestBody ADD
-//    @PostMapping("/add")
-//    public Record addUser() {
-//        return recordService.addRecord(record);
-//    }
+    @PostMapping("/add")
+    public Record addRecord(@RequestBody Record record) {
+        return recordService.addRecord(record);
+    }
+
+    @PostMapping("/update")
+    public Record updateRecord(@RequestBody Record record) {
+        return recordService.update(record);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteRecord(@PathVariable("id") Record record) {
+        recordService.delete(record);
+    }
 }
--- src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/service/RecordService.java
+++ src/main/java/by/bntu/fitr/borzdyko/polyclinic/polyclinic/service/RecordService.java
@@ -3,6 +3,7 @@ package by.bntu.fitr.borzdyko.polyclinic.polyclinic.service;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.model.Record;
 import by.bntu.fitr.borzdyko.polyclinic.polyclinic.repository.RecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,7 +25,19 @@ public class RecordService {
     }
 
     public Record addRecord(Record record) {
+        if(recordRepository.findOne(Example.of(record)).isPresent()) {
+            return null;
+        }
+
         return recordRepository.save(record);
+    }
+
+    public Record update(Record record) {
+        return recordRepository.save(record);
+    }
+
+    public void delete(Record record) {
+        recordRepository.delete(record);
     }
 
 }
